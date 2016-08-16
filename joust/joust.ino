c69bcc7cecaab249eebcb937f0239d61cc89e9a0
Mike MacHenry <mike.machenry@gmail.com>
2016-08-16 00:57:08 -0400

    Adding a multiple life system.

--- joust/joust.ino
+++ joust/joust.ino
@@ -1,115 +1,94 @@
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
 
 // Used for software SPI for accelerometer, Adafruit LIS3DH
 #define LIS3DH_CLK 2
 #define LIS3DH_MISO 0
 #define LIS3DH_MOSI 1
 #define LIS3DH_CS 3
-
-Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
-
-#if defined(ARDUINO_ARCH_SAMD)
-// for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
-   #define Serial SerialUSB
-#endif
-
-// Neopixel libraries
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
-
 #define LED_PIN 4
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
 
-//Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, LED_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, LED_PIN, NEO_RGB + NEO_KHZ800);
+const float gravity = 9.3; // m/s^2, not quite
+const uint32_t white = strip.Color(255,255,255);
+const uint32_t black = strip.Color(0,0,0); 
+const uint32_t red = strip.Color(255,0,0);
+const uint32_t blue = strip.Color(0,0,255);
+const uint32_t green = strip.Color(0,255,0);
+
+// Game play constants.
+const int max_lives = 30;
+const int penalty_rate = 1;
+const int recovery_rate = 2 * penalty_rate;
+const double threshold = 1.0; // lower value means easier to lose
+
+// Global variables.
+int lives = max_lives;
 
 void setup(void) {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
   
   if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
-    blinkTwoColors(strip.Color(255,0,0), strip.Color(0,0,0), 300, 5);
+    blinkTwoColors(red, black, 300, 5);
     while (1);
   }
   lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
-  blinkTwoColors(strip.Color(0,255,0), strip.Color(0,0,0), 300, 5);
+
+  // Count down
+  blinkTwoColors(green, black, 300, 5);
+}
+
+void loop() {
+  displayLives(lives);
+  double magnitude = readMagnitude();
+  int lifeDiff = magnitudeLifeDiff(magnitude);
+  lives = constrain(lives+lifeDiff, 0, max_lives);
+  if (lives == 0) {
+    blinkTwoColors(red, white, 300, 5);
+    setColor(red);
+    while(1);
+  }
+}
+
+int magnitudeLifeDiff (double mag) {
+  if (mag > threshold) {
+    return -1 * penalty_rate;
+  } else {
+    return recovery_rate;
+  }
 }
 
 void blinkTwoColors (uint32_t first, uint32_t second, int duration, int times) {
   for (int i = 0; i < times; i++) {
-    strip.setPixelColor(0,first);
-    strip.show();
+    setColor(first);
     delay(duration);
-    strip.setPixelColor(0, second);
-    strip.show();
+    setColor(second);
     delay(duration);
   }
 }
 
-float gravity = 9.3; // m/s^2, not quite 
-double magnitude = 0;
-double threshhold = 2; // lower value means easier to lose
-bool hasLost = false;
-uint32_t color;
-uint32_t blinkColor = strip.Color(255,255,255);
-unsigned long timeWhenLost;
-unsigned long lastBlinkTime = 0;
-bool hasBlinked = false;
+void setColor (uint32_t color) {
+  strip.setPixelColor(0, color);
+  strip.show();
+}
 
-void loop() {
-  // read accelerometer data
-  lis.read();      // get X Y and Z data at once
-  // Then print out the raw data
-  //Serial.print("X:  "); Serial.print(lis.x); 
-  //Serial.print("  \tY:  "); Serial.print(lis.y); 
-  //Serial.print("  \tZ:  "); Serial.print(lis.z);
+void displayLives(int n) {
+  setColor(strip.Color(255 * (max_lives-n) / max_lives, 0, 255 * n / max_lives));
+}
 
-  /* Or....get a new sensor event, normalized */ 
+double readMagnitude () {
+  lis.read();
   sensors_event_t event; 
   lis.getEvent(&event);
-
-  // get the magnitude of the vectors minus gravity, and use the absolute value of that. or decay magnitude
-  magnitude = max(abs(sqrt(pow(event.acceleration.x,2) + pow(event.acceleration.y,2) + pow(event.acceleration.z,2)) - gravity), magnitude*0.980);
-    
-  // start off blue and smoothly transition to red the harder the controller's been shaken
-  color = strip.Color(map(round(magnitude),0,20,0,255),0,255-map(round(magnitude),0,20,0,255));
-
-  if (magnitude > threshhold && !hasLost) {
-    hasLost = true;
-    timeWhenLost = millis();
-  }
-
-  if (hasLost) {
-    if (millis() - timeWhenLost < 3000 && !hasBlinked) { // the numerical constant changes length of blink mode, should be about 1 second
-      if (millis() - lastBlinkTime > 100) { // the numerical constant changes duration of a color in blink, should be about 100ms
-        // flip back and forth between colors for a bit to indicate lost state
-        blinkColor = blinkColor == strip.Color(255,0,0) ? strip.Color(255,255,255) : strip.Color(255,0,0);
-        lastBlinkTime = millis();  
-      }
-      color = blinkColor;
-      
-      // TODO: start vibrator motor
-      
-    } else {
-      // steady color in lost state
-      hasBlinked = true;
-      color = strip.Color(255,0,0);
-
-      // TODO: stop vibrator motor
-    }
-  }
-
-  strip.setPixelColor(0, color);
-  strip.show();  
+  return abs(sqrt(pow(event.acceleration.x,2)
+                 + pow(event.acceleration.y,2)
+                 + pow(event.acceleration.z,2)) - gravity);
 }
+
